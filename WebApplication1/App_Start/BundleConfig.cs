355d04b0ade120951355d86a41cacb348bc3ce14
Yogesh Khichi <ykhichi@aptean.com>
2023-01-18 15:46:40 +0530

    action results

--- WebApplication1/App_Start/BundleConfig.cs
+++ WebApplication1/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace WebApplication1
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
