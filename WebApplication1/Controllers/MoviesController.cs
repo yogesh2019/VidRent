45d7dfda39c312c6a8839b2415a95edcddd2e38d
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-19 07:23:19 +0530

    View Model

--- WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Models;
+using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers
 {
@@ -12,8 +13,19 @@ namespace WebApplication1.Controllers
         // GET: Movies/Random   
         public ActionResult Random()
         {
-            var movie = new Movies() { Name = "shrek !"};
-            return View(movie);
+            var movie = new Movies() { Name = "shrek !" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name =  "Customer 1"},
+                new Customer { Name = "Customer 2"}
+
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
         [Route("movies/released/{year}/{month}")]
